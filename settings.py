0ee10943595636bf66c96c5ee2e9e21911cfa4d6
oskaragzigitov <oskar.agzigitov@yanex.ru>
2024-12-07 03:05:19 +0300

    Increase in the cost aliens

--- settings.py
+++ settings.py
@@ -24,6 +24,8 @@ class Settings():
 
         # Темп ускорения игры
         self.speedup_scale = 1.1
+        # Рост стоимости пришельцев
+        self.speedup_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -40,7 +42,10 @@ class Settings():
         self.alien_points = 50
 
     def increase_speed(self):
-        """Увеличивает настройки скорости."""
+        """Увеличивает настройки скорости и стоимости пришельцев."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.speedup_scale)
+        # print(self.alien_points)
